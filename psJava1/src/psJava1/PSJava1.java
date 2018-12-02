a268dfc7c36421a4e32fe9c30b86391d20631ba5
pranay <pranaysad@hotmail.com>
2018-12-01 17:22:16 -0800

    Initial Commit

---
+++ psJava1/src/psJava1/PSJava1.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package psJava1;
+
+/**
+ * @author pranay
+ *
+ */
+
+
+
+public class PSJava1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello");
+		
+		
+		
+		
+	}
+
+}
+
